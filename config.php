84d9c626a43c8eee481b8d073063ad3a8e46197f
yui20251124 <koyanagiyui.913@gmail.com>
2026-01-16 12:02:16 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'db' => [
+    'host' => '127.0.0.1',
+    'name' => 'kakeibo',
+    'user' => 'root',
+    'pass' => '',     // XAMPPのデフォルト
+    'charset' => 'utf8mb4',
+  ],
+];
